875358342c94182a0ed4fa30332abe2010d9e0df
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2020-12-24 16:29:56 +0000

    Applied ESLint formatting

--- src/promise-katas-2.js
+++ src/promise-katas-2.js
@@ -1,13 +1,13 @@
-const { fetch } = require("./fakeApi");
-/* 
+const { fetch } = require('./fakeApi');
+/*
 
-DO NOT change the import or names of the functions in this file. 
+DO NOT change the import or names of the functions in this file.
 
-However, you will have to amend the body of the functions themselves in order to make the tests pass. 
+However, you will have to amend the body of the functions themselves in order to make the tests pass.
 
 You can look at the fakeApi.js file to see the shape of the data that is returned from our fakeApi
 
-We have imitated the fetch module for these katas. The fetch function takes two parameters, url and requestBody. 
+We have imitated the fetch module for these katas. The fetch function takes two parameters, url and requestBody.
 
 For the first 3 exercises you will only need to provide the first parameter, url. For the last exercise you will
 need to use the second parameter, requestBody.
@@ -25,33 +25,42 @@ fetch("joke", "question").then(<insert your callback function>)
 // 1 Create a function that uses the getData function to make a request to the "food" URL and returns
 // the data - expected return value "Cheese" of type String
 
-const food = () => {};
+const food = () => fetch('food').then((res) => res.data);
 
 // 2 Create a function that uses the getData function to make a request to the "cats" URL and returns
 // a list of cats in alphabetical order - expected return value ["Bandit", "Berry", "Puss in boots", "Smokey"] of type Array
 
-const cat = () => {};
+const cat = () => fetch('cats').then((res) => res.data.cats.sort());
 
 // 3 Create a function that uses the getData function to make a request to the "dogs" URL and returns
 // the naughtiest dog - expected return value {name: "Mutley", naughty: 10} of type Object
 
-const dog = () => {};
+const dog = () => fetch('dogs').then((res) => {
+  const newArray = res.data.dogs;
+  console.log(newArray);
+  const naughtiestDog = newArray.reduce((previousDog, currentDog) => {
+    if (previousDog.naughty > currentDog.naughty) {
+      return previousDog;
+    }
+    return currentDog;
+  });
+});
 
 // 4 Create a function that uses the getData function to make requests to the "jokes" URL and returns
-// a joke object with the key of question and answer - expected return { 
+// a joke object with the key of question and answer - expected return {
 //     question: "Why did the scarecrow win the Nobel Prize?",
 //     answer: "Because he was out-standing in his field."
 // } of type Object
-// You will have to make more than one request to our fakeApi to get all the data you need. 
+// You will have to make more than one request to our fakeApi to get all the data you need.
 // Be aware of nesting your calls (try to avoid callback hell). Take a look at Promise.all on MDN
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
 //
 
-const joke = () => {};
+const joke = () => fetch('jokes', 'Object');
 
 module.exports = {
-    food,
-    cat,
-    dog,
-    joke
-}
+  food,
+  cat,
+  dog,
+  joke,
+};
